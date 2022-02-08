bf5650d59343f4b1fde9f4343d5279d65f45c93d
Anthony <hososlch@gmail.com>
2022-02-08 14:16:44 +0800

    fix: update settings

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from controllers.home_controller import home_controller
 from controllers.stocks_controller import stocks_controller
@@ -10,4 +11,5 @@ app.register_blueprint(stocks_controller)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
